c0a538e383363bfaf20e9a53114485d457881b7e
lokeshsaini94 <lokeshsaini94@gmail.com>
2017-01-25 01:30:58 +0530

    update 1
    Completed step 0 and step 1 of Build It Bigger - Instructions

---
+++ joker/src/main/java/lokeshsaini/joker/Joker.java
@@ -0,0 +1,15 @@
+package lokeshsaini.joker;
+
+/**
+ * Created by lokeshsaini94 on 25-01-2017.
+ */
+
+public class Joker {
+    public Joker() {
+
+    }
+
+    public String getJoke() {
+        return "How many tickles does it take to make a squid laugh?\nTen-tickles";
+    }
+}
